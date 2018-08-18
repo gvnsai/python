11ce3650128c5f8200a54fbc50a7c3d5fb5a5cac
gvnsai <42485121+gvnsai@users.noreply.github.com>
2018-08-18 10:34:49 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -5,13 +5,4 @@ if string.isdigit():
 	count+=1
 elif type(string) == str:
 	print('it is string')
-	# new_str = string.split(' ')
-	# for i in new_str:
-	# 	if i.isdigit():
-	# 		print('integer in the string')
-	# 		count+= 1
-	# 		break
-	# 	else:
-	# 		pass
-# if count==1:
-# 	print('it is a int')
+	

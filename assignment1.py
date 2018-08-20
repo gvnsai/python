fdb74e39819090f425cc3a09e3a581f65b886192
gvnsai <42485121+gvnsai@users.noreply.github.com>
2018-08-20 23:51:54 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,13 @@
-string = raw_input("please enter string: ")
-count = 0
-if string.isdigit():
-	print('integer')
-	count+=1
-elif type(string) == str:
-	print('it is string')
-	
+# Check input data is string or int or int in the string.
+
+try:
+    input_data=eval(input('Enter u r data:'))
+    if type(input_data) == int:
+        print(input_data,'Enter input is int type only')  
+    elif type(input_data) == str:
+        if input_data.isdigit():
+            print(input_data,'Entered input is int in string')
+        else:
+            print(input_data,'Entered input is sting only')
+except NameError:
+    print('its string')
